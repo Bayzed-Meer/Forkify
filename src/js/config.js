5a58f91a03e08b12d78c7606999520e35945b787
Bayzed-Meer <rsbayzed@gmail.com>
2023-09-16 19:49:31 +0600

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '286484aa-0f1d-4653-9842-465aca14821f';
+export const MODAL_CLOSE_SEC = 2.5;
